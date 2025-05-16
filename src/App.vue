1bf9d019433a0b84ef5317f85634211de1c47627
borjagonz <borjagonz@gmail.com>
2025-05-16 13:05:36 +0200

    hola

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import HeaderComponent from './components/HeaderComponent.vue'
+  import HeadlineComponent from './components/HeadlineComponent.vue'
+  import HighlightsComponent from './components/HighlightsComponent.vue';
+  import NewsletterComponent from './components/NewsletterComponent.vue';
+  import FooterComponent from './components/FooterComponent.vue';
+</script>
+
+<template>
+  <section>
+    <HeaderComponent></HeaderComponent>
+  </section>
+  <section>
+    <HeadlineComponent></HeadlineComponent>
+  </section>
+  <section>
+    <HighlightsComponent></HighlightsComponent>
+  </section>
+  <section>
+    <NewsletterComponent></NewsletterComponent>
+  </section>
+  <section>
+    <FooterComponent></FooterComponent>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <section>
+    <header>
+          <img src="" alt="Logo-Tasky">
+    </header>
+  </section>
+</template>
+
+<style></style>
