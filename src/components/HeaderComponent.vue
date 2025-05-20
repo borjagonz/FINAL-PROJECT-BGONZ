8427d4cc0ebbd1fa1fb2ff6bfe8c6762840466d3
borjagonz <borjagonz@gmail.com>
2025-05-20 10:58:57 +0200

    hola

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,6 +3,7 @@
 
 <template>
   <section>
+    <img src="https://raw.githubusercontent.com/borjagonz/FINAL-PROJECT-BGONZ/refs/heads/main/src/logo/tasky-logo.png" alt="">
     <p>Hola</p>
   </section>
 </template>
